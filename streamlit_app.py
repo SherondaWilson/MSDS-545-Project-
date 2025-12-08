bbddac9d1102804ab0477ea17367a25a0a07d873
SherondaWilson <Sherondawilson1205@gmail.com>
2025-12-07 21:30:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (
-    classification_report,
-    confusion_matrix,
-    roc_auc_score,
-    roc_curve,
-)
+
 
 # ---------------------------------------------------
 # Streamlit page config
